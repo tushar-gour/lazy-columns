15834c9277c1f8b2f209fdc6342e37a7356a7c91
tushar-gour <tushargour004@gmail.com>
2025-02-08 22:42:57 +0530

    update

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -2,7 +2,7 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const healthcheck = asyncHandler(async (_, res) => {
-    res.status(200).json(new ApiResponse(200, {}, "OK"));
+    res.status(200).json(new ApiResponse(200, "OK"));
 });
 
 export default healthcheck;
